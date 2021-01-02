e17032330a116fc9faec070dc26894afac5fa6eb
Mani Nagarathnam <mnn3@illinois.edu>
2021-01-02 15:45:53 -0600

    asd
    asd

--- web_crawler.py
+++ web_crawler.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://champion.gg/champion/Ahri/Middle'
+URL = 'https://champion.gg/champion/Zed/Middle'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='blitz-app')
 champion_runes = results.find_all('div', class_="View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 eidRwp")
+
 for runes in champion_runes:
-    images = runes.findAll('img')
+    images = runes.findAll('img', string = '&lt;itemdata&gt;')
     print(images)
 #print(results.prettify())
 
