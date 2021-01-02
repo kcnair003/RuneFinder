a3daa040d275b9db795523b42b9327059a83b2b2
kcnair003 <kcnair003@gmail.com>
2021-01-02 15:58:48 -0600

    Update webcrawler
    Finds summoner spells and build items in first iteration

--- web_crawler.py
+++ web_crawler.py
@@ -5,7 +5,7 @@ URL = 'https://champion.gg/champion/Ahri/Middle'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find('div', class_="Columns__LayoutColumns-sc-24rxii-0 iwiFuU")
+results = soup.find('div', class_="Columns__Column-sc-24rxii-1 kbeXNP")
 champion_runes = results.find_all('div', class_="View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 eidRwp")
 for runes in champion_runes:
     images = runes.findAll('img')
