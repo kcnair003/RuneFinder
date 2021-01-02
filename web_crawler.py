647c88a792d981aa31b7214cd8e88dd1040d43a1
kcnair003 <kcnair003@gmail.com>
2021-01-02 15:10:06 -0600

    Updated Webcrawler
    Webcrawler now explores Ahri champ page to find the image data within the page

--- web_crawler.py
+++ web_crawler.py
@@ -1,10 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+URL = 'https://champion.gg/champion/Ahri/Middle'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
+results = soup.find(id='blitz-app')
+champion_runes = results.find_all('div', class_="View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 eidRwp")
+for runes in champion_runes:
+    images = runes.findAll('img')
+    print(images)
+#print(results.prettify())
 
-print(results.prettify())
+#for runes in champion_runes:
+#    print(runes, end='\n'*2)
