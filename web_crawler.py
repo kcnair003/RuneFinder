ab1fd61342c3cf7dec94bd6f70c91c133d84c0a3
kcnair003 <kcnair003@gmail.com>
2021-01-02 15:44:47 -0600

    Update web_crawler.py
    Ensures that empty images are not rendered in output

--- web_crawler.py
+++ web_crawler.py
@@ -5,11 +5,12 @@ URL = 'https://champion.gg/champion/Ahri/Middle'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='blitz-app')
+results = soup.find('div', class_="Columns__LayoutColumns-sc-24rxii-0 iwiFuU")
 champion_runes = results.find_all('div', class_="View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 eidRwp")
 for runes in champion_runes:
     images = runes.findAll('img')
-    print(images)
+    if images:
+        print(images)
 #print(results.prettify())
 
 #for runes in champion_runes:
