ce6b7e8161f1cfc2dcd7fa684f6713d54a50ccd8
Mani Nagarathnam <mnn3@illinois.edu>
2021-01-02 14:18:06 -0600

    Update rune_app.py
    Discord import

---
+++ rune_app.py
@@ -0,0 +1,13 @@
+# rune_app.py
+import os
+
+from dotenv import load_dotenv
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
