ce8d513118030ce2482ba59eb59eea0e409d02fd
kcnair003 <kcnair003@gmail.com>
2021-01-03 10:10:29 -0600

    Revert "Update web_crawler.py"
    This reverts commit 0f69f624dda4490812f8acb2fbf910fe7340fb2b.

--- web_crawler.py
+++ web_crawler.py
@@ -6,10 +6,10 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find('div', class_="Columns__Column-sc-24rxii-1 kbeXNP")
-champion_runes = results.find_all('div', class_="sc-eAudoH gPudsA View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 hxNeHv")
+champion_runes = results.find_all('div', class_="View-sc-1c57lgy-1 cLLSJv View__StyledDiv-sc-1c57lgy-0 eidRwp")
 
 for runes in champion_runes:
-    images = runes.findAll('img')
+    images = runes.findAll('img', string=lambda text: 'item' in text.lower())
     if images:
         print(images)
 #print(results.prettify())
