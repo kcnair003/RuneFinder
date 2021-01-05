09abe5e5efa7339c522f8e4ff65a5b71343f7bab
Mani Nagarathnam <mnn3@illinois.edu>
2021-01-05 17:46:34 -0600

    Basic Discord

--- rune_app.py
+++ rune_app.py
@@ -1,27 +1,27 @@
-# rune_app.py
-#import os
-#from dotenv import load_dotenv
-#TOKEN = os.getenv('DISCORD_TOKEN')
-
-#client = discord.Client()
-
-#@client.event
-#async def on_ready():
-#    print(f'{client.user} has connected to Discord!')
-
-#client.run(TOKEN)
-
-
+#rune_app.py
+import os
 
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
 client = discord.Client()
-Token=''
+
 @client.event
-async def onmessage(message):
-    message.content.lower()
-    if message.author == client.user:
-        return
-    if message.content.startswith("Hello"):
-        await message.channel.send("Hello, I am a test bot.")
-    
-client.run(Token)
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
