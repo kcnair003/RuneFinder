36630518454353d7951f4e38b209a162e3d4a028
Ketu-Patel <ketupatel1010@gmail.com>
2021-01-05 05:00:53 -0600

    Update for discord
    Working with python 3.6 where it works with discord.py. Still need tthe token to make sure everything works and I commented the previous code that was written. There is also some sample code that we can go off of.

--- rune_app.py
+++ rune_app.py
@@ -1,13 +1,27 @@
 # rune_app.py
-import os
+#import os
+#from dotenv import load_dotenv
+#TOKEN = os.getenv('DISCORD_TOKEN')
 
-from dotenv import load_dotenv
-TOKEN = os.getenv('DISCORD_TOKEN')
+#client = discord.Client()
 
+#@client.event
+#async def on_ready():
+#    print(f'{client.user} has connected to Discord!')
+
+#client.run(TOKEN)
+
+
+
+import discord
 client = discord.Client()
-
+Token=''
 @client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-client.run(TOKEN)
+async def onmessage(message):
+    message.content.lower()
+    if message.author == client.user:
+        return
+    if message.content.startswith("Hello"):
+        await message.channel.send("Hello, I am a test bot.")
+    
+client.run(Token)
